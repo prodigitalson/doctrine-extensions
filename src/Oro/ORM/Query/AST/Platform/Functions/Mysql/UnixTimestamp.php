185e25f61c12f1162cc240e46aa71be9c9364944
Anthony Cunningham <anthony_cunningham@anfcorp.com>
2017-02-11 18:23:27 -0500

    Adding UNIX_TIMESTAMP function.

---
+++ src/Oro/ORM/Query/AST/Platform/Functions/Mysql/UnixTimestamp.php
@@ -0,0 +1,20 @@
+<?php
+namespace Oro\ORM\Query\AST\Platform\Functions\Mysql;
+
+use Doctrine\ORM\Query\AST\Node;
+use Doctrine\ORM\Query\SqlWalker;
+use Oro\ORM\Query\AST\Functions\SimpleFunction;
+use Oro\ORM\Query\AST\Platform\Functions\PlatformFunctionNode;
+
+class UnixTimestamp extends PlatformFunctionNode {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSql(SqlWalker $sqlWalker) {
+        /** @var Node $expression */
+        $expression = $this->parameters[SimpleFunction::PARAMETER_KEY];
+        return 'UNIX_TIMESTAMP(' . $this->getExpressionValue($expression, $sqlWalker) . ')';
+    }
+
+}
